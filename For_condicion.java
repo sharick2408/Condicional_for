b66da0acf581ac1a291da4a5014437b07732bd8e
AKgalv1x <akgalvis@gmail.com>
2025-04-02 08:09:33 -0500

    ForSharick1

---
+++ For_condicion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.for_condicion;
+
+/**
+ *
+ * @author Estudiantes  
+ */
+public class For_condicion {
+
+    public static void main(String[] args) {
+        String bebidas[] = {"tinto", "gaseosa", "chocolate"};
+        
+        for (int i=1; i < bebidas.length;i++){
+            System.out.println(bebidas[i]);
+        }
+        
+    }
+}
